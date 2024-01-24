9cc0e0a6edbd4e1259f2ed2a5ca371cbe95a96c4
lizsdl <106372748+lizsdl@users.noreply.github.com>
2024-01-24 15:41:05 +0800

    Update demo.cc

--- Redis++Demo/demo.cc
+++ Redis++Demo/demo.cc
@@ -12,20 +12,11 @@ void messageCallback(const std::string &channel, const std::string &message)
 
 int main()
 {
-
-    // 在这里可以执行其他逻辑，订阅事件循环会在后台运行
-    // 无限循环，保持订阅事件处理
-
     try
     {
         // 创建 Redis 对象
-        // sw::redis::Redis redis("tcp://127.0.0.1:6379");
 
         std::shared_ptr<sw::redis::Redis> redis_;
-
-        std::string url("tcp://127.0.0.1:6379");
-        // sw::redis::ConnectionOptions conn_opts(url);
-
         sw::redis::ConnectionOptions connection_options;
         // 设置 Redis 服务器的连接信息
         connection_options.host = "127.0.0.1";
@@ -35,7 +26,8 @@ int main()
 
         std::shared_ptr<sw::redis::Subscriber> subscriber_;
         subscriber_ = std::make_shared<sw::redis::Subscriber>(redis_->subscriber());
-
+        
+        // 使用 on_message 设置消息处理函数
         subscriber_->on_message(std::bind(messageCallback, std::placeholders::_1, std::placeholders::_2));
 
         std::cout << "Before subscribe" << std::endl;
@@ -43,11 +35,6 @@ int main()
         subscriber_->subscribe("mychannel");
 
         std::cout << "After subscribe" << std::endl;
-
-        // 使用 on_message 设置消息处理函数
-
-        std::cout << "Test  " << std::endl;
-
 
         // 这里需要一直循环调用consume，用于处理接收到的消息队列中的消息。这样可以确保及时处理订阅频道上的消息，并且保持订阅事件循环一直在运行。
         while(true){
